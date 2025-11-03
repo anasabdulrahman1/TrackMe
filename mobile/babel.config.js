b55dc80ac1083bbbe4abe3aa4d5862985d6a94c5
anasabdulrahman1 <anas777va@gmail.com>
2025-11-03 13:37:20 +0530

    check

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  // ADD THIS PLUGINS ARRAY
+  plugins: [
+    'react-native-dotenv',
+  ],
   env: {
     production: {
       plugins: ['react-native-paper/babel'],
--- mobile/src/lib/supabase.ts
+++ mobile/src/lib/supabase.ts
@@ -1,15 +1,19 @@
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
-import Config from 'react-native-config';
 
-if (!Config.SUPABASE_URL || !Config.SUPABASE_ANON_KEY) {
+// 1. Import keys directly from @env
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env'; 
+
+// 2. Add a Typescript check
+if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
   throw new Error(
     'Missing environment variables. Check that you have .env file with SUPABASE_URL and SUPABASE_ANON_KEY'
   );
 }
 
-export const supabase = createClient(Config.SUPABASE_URL, Config.SUPABASE_ANON_KEY, {
+// 3. Use the variables directly (no "Config." prefix)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
